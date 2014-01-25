f6d8ed54a53ff993d927ebe65181624911675c74
czervos <czervos@ualberta.ca>
2014-01-24 21:07:33 -0700

    Created AddCounterActivity

--- gen/ca/ualberta/cmput301/as1/czervos_notes/R.java
+++ gen/ca/ualberta/cmput301/as1/czervos_notes/R.java
@@ -25,20 +25,24 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080001;
         public static final int counterList=0x7f080000;
-        public static final int counter_add=0x7f080001;
+        public static final int counter_add=0x7f080002;
     }
     public static final class layout {
-        public static final int activity_counter_list=0x7f030000;
+        public static final int activity_add_counter=0x7f030000;
+        public static final int activity_counter_list=0x7f030001;
     }
     public static final class menu {
-        public static final int counter_list=0x7f070000;
+        public static final int add_counter=0x7f070000;
+        public static final int counter_list=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int counter_add=0x7f050002;
+        public static final int hello_world=0x7f050004;
+        public static final int title_activity_add_counter=0x7f050003;
     }
     public static final class style {
         /** 
--- src/ca/ualberta/cmput301/as1/czervos_notes/CounterListActivity.java
+++ src/ca/ualberta/cmput301/as1/czervos_notes/CounterListActivity.java
@@ -19,6 +19,7 @@ package ca.ualberta.cmput301.as1.czervos_notes;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * The main activity of the application.
@@ -39,6 +40,28 @@ public class CounterListActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.counter_list, menu);
 		return true;
+	}
+	
+	/**
+	 * Responds to action button presses
+	 * CODE REUSE:
+	 * This code was modified from
+	 * URL: http://developer.android.com/training/basics/actionbar/adding-buttons.html#Respond
+	 * Date: Jan. 24, 2014
+	 * License: Creative Commons 2.5 Attribution License (http://creativecommons.org/licenses/by/2.5/)
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.counter_add:
+	            // TODO add counter functionality
+	            return true;
+	        case R.id.action_settings:
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
 	}
 
 }
