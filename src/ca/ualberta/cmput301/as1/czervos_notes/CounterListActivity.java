30a6620c0362cbef19f94f397dc76b4d5635ad21
czervos <czervos@ualberta.ca>
2014-01-25 18:48:20 -0700

    Implemented counter list persistence
    Saving implemented using JSON, GSON, and Shared preferences.

--- src/ca/ualberta/cmput301/as1/czervos_notes/CounterListActivity.java
+++ src/ca/ualberta/cmput301/as1/czervos_notes/CounterListActivity.java
@@ -17,14 +17,20 @@
 package ca.ualberta.cmput301.as1.czervos_notes;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.renderscript.Type;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 /**
  * The main activity of the application.
@@ -41,7 +47,7 @@ public class CounterListActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_counter_list);
-		counterList = new CounterListModel();
+		//counterList = new CounterListModel();
 		// Creates the list containing the counters
 	}
 
@@ -58,6 +64,12 @@ public class CounterListActivity extends Activity {
 		CounterModel newCounter;
 		// Defines a new counter
 		
+		counterList = loadCounterList();
+		
+		if (counterList == null){
+			counterList = new CounterListModel();
+		}
+		
 		/* Updates counter list if needed */
 		Intent intent = this.getIntent();
 		// Gets the intent
@@ -69,6 +81,11 @@ public class CounterListActivity extends Activity {
 			// Gets the new counter from the bundle
 			counterList.addCounter(newCounter);
 			// Adds the counter to the list
+			
+			saveCounterList(counterList);
+			// Saves the list
+			intent.removeExtra("newCounter");
+			// Removes the bundle when done with it
 		}
 		
 		/* Displays List of Counters */
@@ -112,6 +129,63 @@ public class CounterListActivity extends Activity {
 	        default:
 	            return super.onOptionsItemSelected(item);
 	    }
+	}
+	
+	/**
+	 * Method takes in the counter list model, and saves the counters in the 
+	 * counter list.
+	 * 
+	 * @param counterListModel
+	 */
+	public void saveCounterList(CounterListModel counterListModel) {
+		ArrayList<CounterModel> counterList = new ArrayList<CounterModel>();
+		// initializes an array to store counters
+		counterList = counterListModel.getCounterList();
+		// gets the list of counters from the counterListModel
+		Gson gson = new Gson();
+		// initializes gson
+		String json = gson.toJson(counterList);
+		// converts list of counters to a JSON string object
+		
+		
+		SharedPreferences savedList = getPreferences(MODE_PRIVATE);
+		// initializes shared preferences
+		SharedPreferences.Editor editor = savedList.edit();
+		// initializes shared preferences editor
+		editor.putString("savedList",json);
+		// saves the JSON string object as "savedList"
+		editor.commit();
+		// commits the save to shared preferences
+	}
+	
+	/**
+	 * Method loads the stored array list and returns the counterListModel
+	 * with the loaded counters. If there is nothing saved, returns null.
+	 * 
+	 * @return
+	 */
+	public CounterListModel loadCounterList() {
+		Gson gson = new Gson();
+		// initializes gson
+		CounterListModel counterListModel = new CounterListModel();
+		// initializes counter list model
+		SharedPreferences savedList = getPreferences(MODE_PRIVATE);
+		// initializes shared preferences
+		String json = savedList.getString("savedList","noValue");
+		// loads the JSON string object saved as "savedList"
+		// returns "noValue" if no JSON string object located 
+		if (json == "noValue") {
+			return null;
+			// method returns null if no JSON string object was located
+		}
+		
+		java.lang.reflect.Type collectionType = new TypeToken<ArrayList<CounterModel>>(){}.getType();
+		// Sets collection type to ArrayList<CounterModel>
+		ArrayList<CounterModel> loadedList = gson.fromJson(json, collectionType);
+		// converts JSON string object to ArrayList<CounterModel>
+		counterListModel.setCounterList(loadedList);
+		// sets counter list in model to the loaded list
+		return counterListModel;		
 	}
 
 }
